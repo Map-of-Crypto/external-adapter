85cae5dca677878bb0f28fdbcb49c99ad8f9d474
Your Name <you@example.com>
2022-04-30 17:23:36 -0500

    add api moralis

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { Requester, Validator } = require('@chainlink/external-adapter')
 
+const Moralis = require("moralis/node")
+
+const MapOfCrypto = require("./MapOfCrypto.json")
+
 // Define custom error scenarios for the API.
 // Return true for the adapter to retry.
 const customError = (data) => {
@@ -18,12 +22,47 @@ const customError = (data) => {
 // }
 
 // First we will call the tracking API.. we will return the list of all products with the status
-const createRequest = (input, callback) => {
+const createRequest = async (input, callback) => {
   // The Validator helps you validate the Chainlink request data
+
+  const serverUrl = "https://pmdvzjdaz3qr.usemoralis.com:2053/server";
+  const appId = "x0oyqC34SORc7FpG2VH9Byu7nHe6350m6nyNLgo6";
+  const masterKey = "a2Dl0Chlb4JlaUOBihwzevG3PuKIyQQ1jj0YQCnN";
+
+  await Moralis.start({ serverUrl, appId, masterKey });
+
+  const ABI = MapOfCrypto.abi;
+
+  const options = {
+    chain: "kovan",
+    address: "0xcb2E631887B15B815AD02aE88Dc0326374f37b16",
+    function_name: "returnRequestedPurchaseList",
+    abi: ABI,
+    // params: { who: "0x3355d6E71585d4e619f4dB4C7c5Bfe549b278299" },
+  };
+
+  const pendingRequests = await Moralis.Web3API.native.runContractFunction(options);
+
   const validator = new Validator(callback, input)
   const jobRunID = validator.validated.id
+
+
+
+  let pendingFundingList = [];
+  for (let i = 0; i < pendingRequests.length; i++) {
+    // confirmed but not paid 
+    if (!pendingRequests[i][0] && pendingRequests[i][2]) {
+      let pendingFunding = {
+        paid: pendingRequests[i][0],
+        id: pendingRequests[i][1],
+        confirmed: pendingRequests[i][2]
+      }
+      pendingFundingList.push(pendingFunding);
+    }
+  }
+
   // const endpoint = validator.validated.data.endpoint || 'price'
-  const url = `https://mapofcrypto-cdppi36oeq-uc.a.run.app/trackingNeedFunding`
+  const url = `https://mapofcrypto-cdppi36oeq-uc.a.run.app/tracking`
   // const fsym = validator.validated.data.base.toUpperCase()
   // const tsyms = validator.validated.data.quote.toUpperCase()
 
@@ -46,12 +85,22 @@ const createRequest = (input, callback) => {
   // or connection failure
   Requester.request(config, customError)
     .then(response => {
-      // It's common practice to store the desired value at the top-level
-      // result key. This allows different adapters to be compatible with
-      // one another.
-      // response.data.result = Requester.validateResultNumber(response.data, [tsyms])
-      // we will save the list of products
-      // createRequest2(input, callback)
+
+      const trackingApiResponse = response.data.tracking;
+
+      const trackingFiltered = trackingApiResponse.filter(tracking => tracking["status"] = "Delivered");
+
+      let productIdFunding = [];
+      for (let i = 0; i < pendingFundingList.length; i++) {
+        let id = pendingFundingList[i]["id"];
+        const find = trackingFiltered.find(element => element.productId === id)
+        if (find) {
+          productIdFunding.push(find.productId);
+        }
+      }
+
+      response.data = productIdFunding;
+
       callback(response.status, Requester.success(jobRunID, response))
     })
     .catch(error => {
@@ -59,46 +108,7 @@ const createRequest = (input, callback) => {
     })
 }
 
-// here we will connect to the blockchain and read from the request object
-const createRequest2 = (input, callback) => {
-  // The Validator helps you validate the Chainlink request data
-  const validator = new Validator(callback, input, customParams)
-  const jobRunID = validator.validated.id
-  const endpoint = validator.validated.data.endpoint || 'price'
-  const url = `https://min-api.cryptocompare.com/data/${endpoint}`
-  const fsym = validator.validated.data.base.toUpperCase()
-  const tsyms = validator.validated.data.quote.toUpperCase()
 
-  const params = {
-    fsym,
-    tsyms
-  }
-
-  // This is where you would add method and headers
-  // you can add method like GET or POST and add it to the config
-  // The default is GET requests
-  // method = 'get' 
-  // headers = 'headers.....'
-  const config = {
-    url,
-    params
-  }
-
-  // The Requester allows API calls be retry in case of timeout
-  // or connection failure
-  Requester.request(config, customError)
-    .then(response => {
-      // It's common practice to store the desired value at the top-level
-      // result key. This allows different adapters to be compatible with
-      // one another.
-      response.data.result = Requester.validateResultNumber(response.data, [tsyms])
-      callback(response.status, Requester.success(jobRunID, response))
-      // createRequest2(input, callback)
-    })
-    .catch(error => {
-      callback(500, Requester.errored(jobRunID, error))
-    })
-}
 
 // This is a wrapper to allow the function to work with
 // GCP Functions
